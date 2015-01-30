2d3b2d7b3f78aecc0a42cdc846f84fb671e9d5b7
Nate Cook <nathaniel@insight.ly>
2015-01-30 17:53:51 -0300

    Added hash (for comment anchors) and updated to avoid a crash that was happening. (Now it waits until the diff is loaded before reloading the page, which avoids the crash.)

--- chrome-extension/diff-fix.js
+++ chrome-extension/diff-fix.js
@@ -1,30 +1,37 @@
+var fullyLoadedCheckInterval = 300;
 
-;(function(){
+function setLocationWhenPageFullyLoaded(newLocation) {
+	var pullRequestLoaded = !!document.getElementById("pullrequest-diff");
 
-	var search = location.search.length
-		? location.search.replace('?', '').split('&')
-		: []
-	;
-
-	var tsParam	= 'ts=4';
-	var tsIndex	= search.indexOf(tsParam);
-	var wsParam	= 'w=1';
-	var wsIndex	= search.indexOf(wsParam);
-
-	var searchChanged = false;
-
-	if (tsIndex == -1) {
-		search.push(tsParam);
-		searchChanged = true;
-	}
-
-	if (wsIndex == -1) {
-		search.push(wsParam);
-		searchChanged = true;
-	}
-
-	if (searchChanged == true) {
-		location.href = location.pathname + '?' + search.join('&');
-	}
+	if (pullRequestLoaded == true)
+		location.href = newLocation;
+	else
+		setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, fullyLoadedCheckInterval);
+}
 	
-})();
+var queryParameters = location.search.length
+	? location.search.replace('?', '').split('&')
+	: []
+;
+
+var tabsParameter = 'ts=4';
+var tabsIndex = queryParameters.indexOf(tabsParameter);
+var whiteSpaceParameter	= 'w=1';
+var whiteSpaceIndex = queryParameters.indexOf(whiteSpaceParameter);
+
+var parametersChanged = false;
+
+if (tabsIndex == -1) {
+	queryParameters.push(tabsParameter);
+	parametersChanged = true;
+}
+
+if (whiteSpaceIndex == -1) {
+	queryParameters.push(whiteSpaceParameter);
+	parametersChanged = true;
+}
+
+if (parametersChanged == true) {
+	var newLocation = location.pathname + '?' + queryParameters.join('&') + location.hash;		
+	setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, fullyLoadedCheckInterval);
+}	
