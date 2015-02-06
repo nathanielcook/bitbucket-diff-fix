c9db6781533385f1234a66583eaf918c2503fc15
Nate Cook <nathaniel@insight.ly>
2015-02-06 14:59:30 -0300

    I wasn't preserving the hash for the comments-link and likes-link links on the commit pages. Realized I needed to parse the URL rather than just using concatenation, so refactored the existing code to parse it the same way every time.

--- chrome-extension/diff-fix.js
+++ chrome-extension/diff-fix.js
@@ -1,5 +1,4 @@
 var checkInterval = 300;
-var paramString = "?ts=4&w=1";
 
 function setLocationWhenPageFullyLoaded(newLocation) {
 	var retry = false;
@@ -23,54 +22,37 @@ function setLocationWhenPageFullyLoaded(newLocation) {
 		location.href = newLocation;		
 }
 
-function addParametersToPullRequestLinks() {
+function addParametersToBitbucketLinks(urlKey) {
     var elems = document.getElementsByTagName('a');
     var count = elems.length;
     for (var i = 0; i < count; i++) {
-        if((' ' + elems[i].className + ' ').indexOf(' execute ') > -1
+        if ((' ' + elems[i].className + ' ').indexOf(' execute ') > -1
         || (' ' + elems[i].className + ' ').indexOf(' comments-link ') > -1
         || (' ' + elems[i].className + ' ').indexOf(' likes-link ') > -1) {
-			if (elems[i].href.indexOf("/pull-request/") > -1 && elems[i].href.indexOf(paramString) == -1)
-				elems[i].href = elems[i].href + paramString;
+			if (elems[i].href.indexOf(urlKey) > -1) {
+				var newUrl = getNewUrl(elems[i].href);
+				if (newUrl != elems[i].href)
+					elems[i].href = newUrl;
+			}
         }
     }
-    
+}
+
+function addParametersToPullRequestLinks() {
+	addParametersToBitbucketLinks("/pull-request/");    
     setTimeout(function(){ addParametersToPullRequestLinks(); }, checkInterval);
 }
 
 function addParametersToCommitLinks() {
-    var elems = document.getElementsByTagName('a');
-    var count = elems.length;
-    for (var i = 0; i < count; i++) {
-        if((' ' + elems[i].className + ' ').indexOf(' hash execute ') > -1
-        || (' ' + elems[i].className + ' ').indexOf(' comments-link ') > -1
-        || (' ' + elems[i].className + ' ').indexOf(' likes-link ') > -1) {
-			if (elems[i].href.indexOf("/commits/") > -1 && elems[i].href.indexOf(paramString) == -1)
-				elems[i].href = elems[i].href + paramString;
-        }
-    }
-    
+	addParametersToBitbucketLinks("/commits/");    
     setTimeout(function(){ addParametersToCommitLinks(); }, checkInterval);
 }
 
-if (location.href.indexOf("/pull-requests") > -1) {	// if viewing the main pull requests page
-	var createPullRequestLink = document.getElementById("create-pull-request-contextual");
-	
-	// modify "Create pull request" link
-	if (!!createPullRequestLink == true) {
-		var suffix = "/pull-request/new";
-		if (createPullRequestLink.href.indexOf(suffix, this.length - suffix.length) !== -1) // i.e. if (string.endsWith(suffix))
-			createPullRequestLink.href = createPullRequestLink.href + paramString;
-	}
-	
-	setTimeout(function(){ addParametersToPullRequestLinks(); }, checkInterval);
-}
-else { // viewing any other page, pull request diff, new pull request, update pull request or a commit page
-	var queryParameters = location.search.length
-		? location.search.replace('?', '').split('&')
-		: []
-	;
+function getNewUrl(url) {
+	var parser = document.createElement('a');
+	parser.href = url;
 
+	var queryParameters = parser.search.length ? parser.search.replace('?', '').split('&') : [];
 	var tabsParameter = 'ts=4';
 	var tabsIndex = queryParameters.indexOf(tabsParameter);
 	var whiteSpaceParameter	= 'w=1';
@@ -88,11 +70,32 @@ else { // viewing any other page, pull request diff, new pull request, update pu
 		parametersChanged = true;
 	}
 
-	if (parametersChanged == true) {
-		var newLocation = location.pathname + '?' + queryParameters.join('&') + location.hash;		
-		setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, checkInterval);
+	var newUrl = url;
+
+	if (parametersChanged == true)
+		newUrl = parser.pathname + '?' + queryParameters.join('&') + parser.hash;		
+	
+	return newUrl;
+}
+
+if (location.href.indexOf("/pull-requests") > -1) {	// if viewing the main pull requests page
+	var createPullRequestLink = document.getElementById("create-pull-request-contextual");
+	
+	// modify "Create pull request" link
+	if (!!createPullRequestLink == true) {
+		var suffix = "/pull-request/new";
+		if (createPullRequestLink.href.indexOf(suffix, this.length - suffix.length) !== -1) // i.e. if (string.endsWith(suffix))
+			createPullRequestLink.href = getNewUrl(createPullRequestLink.href);
 	}
 	
+	setTimeout(function(){ addParametersToPullRequestLinks(); }, checkInterval);
+}
+else { // viewing any other page, pull request diff, new pull request, update pull request or a commit page	
 	if (location.href.indexOf("/commits/") == -1)
 		setTimeout(function(){ addParametersToCommitLinks(); }, checkInterval);
+
+	var newLocation = getNewUrl(location.href);
+	
+	if (newLocation != location.href)
+		setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, checkInterval);	
 }
