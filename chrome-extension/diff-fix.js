a0fe51fe8e59c3aaea7209c5c2303ce5f86f3fbf
Nate Cook <nathaniel@insight.ly>
2015-02-06 14:03:13 -0300

    Now modifies link on main pull requests page. This eliminates delay for page reload if you're navigating from the main pull requests page. Bumped version to 1.5.

--- chrome-extension/diff-fix.js
+++ chrome-extension/diff-fix.js
@@ -1,4 +1,5 @@
-var fullyLoadedCheckInterval = 300;
+var checkInterval = 300;
+var paramString = "?ts=4&w=1";
 
 function setLocationWhenPageFullyLoaded(newLocation) {
 	if (location.href.indexOf("/pull-request/") > -1)
@@ -8,35 +9,66 @@ function setLocationWhenPageFullyLoaded(newLocation) {
 		if (pullRequestLoaded == true)
 			location.href = newLocation;
 		else
-			setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, fullyLoadedCheckInterval);
+			setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, checkInterval);
 	}
 	else
 		location.href = newLocation;
 }
+
+function addParametersToPullRequestLinks() {
+	var matchClass = "execute";
+    var elems = document.getElementsByTagName('a');
+    var count = elems.length;
+    for (var i = 0; i < count; i++) {
+        if((' ' + elems[i].className + ' ').indexOf(' execute ') > -1
+        || (' ' + elems[i].className + ' ').indexOf(' comments-link ') > -1
+        || (' ' + elems[i].className + ' ').indexOf(' likes-link ') > -1) {
+			if (elems[i].href.indexOf("/pull-request/") > -1 && elems[i].href.indexOf(paramString) == -1)
+				elems[i].href = elems[i].href + paramString;
+        }
+    }
+    
+    setTimeout(function(){ addParametersToPullRequestLinks(); }, checkInterval);
+}
+
+if (location.href.indexOf("/pull-requests") > -1) {	
+	var createPullRequestLink = document.getElementById("create-pull-request-contextual");
 	
-var queryParameters = location.search.length
-	? location.search.replace('?', '').split('&')
-	: []
-;
-
-var tabsParameter = 'ts=4';
-var tabsIndex = queryParameters.indexOf(tabsParameter);
-var whiteSpaceParameter	= 'w=1';
-var whiteSpaceIndex = queryParameters.indexOf(whiteSpaceParameter);
-
-var parametersChanged = false;
-
-if (tabsIndex == -1) {
-	queryParameters.push(tabsParameter);
-	parametersChanged = true;
+	// modify "Create pull request" link
+	if (!!createPullRequestLink == true) {
+		var suffix = "/pull-request/new";
+		if (createPullRequestLink.href.indexOf(suffix, this.length - suffix.length) !== -1) // i.e. if (string.endsWith(suffix))
+			createPullRequestLink.href = createPullRequestLink.href + paramString;
+	}
+	
+	// modify the other links
+	setTimeout(function(){ addParametersToPullRequestLinks(); }, checkInterval);
 }
+else {
+	var queryParameters = location.search.length
+		? location.search.replace('?', '').split('&')
+		: []
+	;
 
-if (whiteSpaceIndex == -1) {
-	queryParameters.push(whiteSpaceParameter);
-	parametersChanged = true;
+	var tabsParameter = 'ts=4';
+	var tabsIndex = queryParameters.indexOf(tabsParameter);
+	var whiteSpaceParameter	= 'w=1';
+	var whiteSpaceIndex = queryParameters.indexOf(whiteSpaceParameter);
+
+	var parametersChanged = false;
+
+	if (tabsIndex == -1) {
+		queryParameters.push(tabsParameter);
+		parametersChanged = true;
+	}
+
+	if (whiteSpaceIndex == -1) {
+		queryParameters.push(whiteSpaceParameter);
+		parametersChanged = true;
+	}
+
+	if (parametersChanged == true) {
+		var newLocation = location.pathname + '?' + queryParameters.join('&') + location.hash;		
+		setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, checkInterval);
+	}	
 }
-
-if (parametersChanged == true) {
-	var newLocation = location.pathname + '?' + queryParameters.join('&') + location.hash;		
-	setTimeout(function(){ setLocationWhenPageFullyLoaded(newLocation); }, fullyLoadedCheckInterval);
-}	
